92c78ea1b38fdb7dbaf85c2ef341c8f6c5006d4b
Alex Truong <Alex@Alexs-MacBook-Pro-3.local>
2021-12-25 15:49:55 -0800

    first commit

---
+++ src/components/logs/LogItem.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Moment from "react-moment"
+import PropTypes from 'prop-types'
+
+const LogItem = ({ log }) => {
+    return (
+        <li className="collection-item">
+            <div>
+                <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? "red-text" : "blue-text"
+                    }`}
+                >
+                    {log.message}
+                </a>
+                <span className="grey-text">
+                    <span className="black-text">{log.tech}</span>
+                </span>
+            </div>
+        </li>
+    )
+}
+
+LogItem.propTypes = {
+    log: PropTypes.object.isRequired
+}
+
+export default LogItem
